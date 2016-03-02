2be9804c4ee9cfc815e98fb8341b6090de4dfe9c
networkturf <networkturf@gmail.com>
2016-03-01 18:16:09 -0800

    changed hostnames

--- show_interfaces.py
+++ show_interfaces.py
@@ -5,16 +5,16 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 cmd='show int desc'
 
 
-host1='rt11bo01'
-ssh.connect(host1, port=22, username='kvaidya', password='shocKwave)3', allow_agent=False,look_for_keys=False)
+host1='router1'
+ssh.connect(host1, port=22, username='cisco', password='cisco', allow_agent=False,look_for_keys=False)
 stdin,stdout,stderr = ssh.exec_command(cmd)
-output_rt11bo01=stdout.readlines()
-print '\n' + host1 + "                       "+ cmd +'\n'.join(output_rt11bo01)
+output_router1=stdout.readlines()
+print '\n' + host1 + "                       "+ cmd +'\n'.join(output_router1)
 
 print '\n' + "******************************************************************************************"
 
-host2='rt11bo02'
-ssh.connect(host2, port=22, username='kvaidya', password='shocKwave)3', allow_agent=False,look_for_keys=False)
+host2='router2'
+ssh.connect(host2, port=22, username='cisco', password='cisco', allow_agent=False,look_for_keys=False)
 stdin,stdout,stderr = ssh.exec_command(cmd)
-output_rt11bo02=stdout.readlines()
-print '\n' + host2 + "                       "+ cmd +'\n'.join(output_rt11bo02)  + '\n'
+output_router2=stdout.readlines()
+print '\n' + host2 + "                       "+ cmd +'\n'.join(output_router2)  + '\n'
